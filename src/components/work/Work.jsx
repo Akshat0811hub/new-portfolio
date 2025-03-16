efe1e2febefcc69c3907acc8c85383ca5dcd5dcd
Akshat Singh <singhakshat875@gmail.com>
2025-03-16 23:41:20 +0530

    made 3 components of work

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import '../work/Work.css'
-
+import React from "react";
+import "../work/Work.css";
 
 const Work = ({ title, text }) => (
-    <div className="work">
-      <div className="work-title">
-        <h1>{title}</h1>
+  <div className="work-main">
+    <div className="work-title">
+      <h1>{title}</h1>
+      <div className="work-text">
         <p>{text}</p>
       </div>
     </div>
-  );
+  </div>
+);
 
-export default Work
+export default Work;
--- src/pages/Value.jsx
+++ src/pages/Value.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import Values from "../components/value/Values";
 import Work from "../components/work/Work";
+import '../css/Value.css'
 
 const Value = () => (
   <div className="main-value-input">
     <div className="value-heading">
       <Values
         title="Value in the lives of others"
-        text="I am driven by a desire to improve the lives of others. My passion lies in enhancing user  experiences and leading projects towards positive outcomes. I believe in a human-centered approach that unlocks business value by providing genuine customer value."/>
+        text="I am driven by a desire to improve the lives of others. My passion lies in enhancing user  experiences and leading projects towards positive outcomes. I believe in a human-centered approach that unlocks business value by providing genuine customer value." />
     </div>
     <div className="work-heading">
-        <Work
-            title="Work"
-            text= "I am a designer who uses design tools to effectively communicate ideas and desired outcomes to achieve a shared understanding. Collaboration is key to my work, and I am an active listener who works effectively with stakeholders from diverse disciplines to achieve the best possible outcome."/>
+      <Work
+        title="Work"
+        text="I am a designer who uses design tools to effectively communicate ideas and desired outcomes to achieve a shared understanding. Collaboration is key to my work, and I am an active listener who works effectively with stakeholders from diverse disciplines to achieve the best possible outcome."/>
     </div>
+    <div className="work-heading">
+      <Work
+        title="Work"
+        text="I am a designer who uses design tools to effectively communicate ideas and desired outcomes to achieve a shared understanding. Collaboration is key to my work, and I am an active listener who works effectively with stakeholders from diverse disciplines to achieve the best possible outcome."/>
+    </div>
+    <div className="work-heading">
+      <Work
+        title="Work"
+        text="I am a designer who uses design tools to effectively communicate ideas and desired outcomes to achieve a shared understanding. Collaboration is key to my work, and I am an active listener who works effectively with stakeholders from diverse disciplines to achieve the best possible outcome."/>
+    </div>
+
   </div>
 );
 
